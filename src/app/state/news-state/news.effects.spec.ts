209462e1f9022cb8f57c64e4dc8e88d0424cff53
Kursat Coskun <kursatcoskun.esogu@gmail.com>
2020-01-04 22:51:53 +0300

    Delete Spec Files

--- src/app/state/news-state/news.effects.spec.ts
+++
@@ -1,7 +0,0 @@
-import { News.Effects } from './news.effects';
-
-describe('News.Effects', () => {
-  it('should create an instance', () => {
-    expect(new News.Effects()).toBeTruthy();
-  });
-});
